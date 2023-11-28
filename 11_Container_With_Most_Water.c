f92ff15bba410755215e869c2f2b94a7a606f8ea
Sucan6 <632190820@qq.com>
2023-11-28 22:44:28 +0800

    LeetCode

--- 11_Container_With_Most_Water.c
+++ 11_Container_With_Most_Water.c
@@ -1,11 +1,11 @@
 int maxArea(int* height, int heightSize) {
     int left = 0,right = heightSize-1;
-    int area,temp,minside;
+    int area = 0,temp = 0,minside = 0;
     while(left < right )
     {
         if(height[left] < height[right]) minside = height[left++];
         else minside = height[right--];
-        area = minside*(right-left);
+        area = minside*(right-left+1);
         if(area > temp) temp = area;
     }
     return temp;
